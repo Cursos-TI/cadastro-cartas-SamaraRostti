cb4df9e69a13927f603e997fe3f28df73ccbb129
SamaraRostti <samara.rostti@gmail.com>
2026-06-27 14:58:08 +0000

    IMPRIMIR OQUE O USUARIO SELECIONOU

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,13 +8,23 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   //ADICIONEI AS VARIÁVEIS NO CODIGO
+  char Estado;
+  char Cod_carta[4];
   int Populacao, Num_pontos_P;
   float Area;
   float PIB;
 
 
   // Área para entrada de dados
+
   //COLOQUEI O PREENCHIMENTO DOS DADOS
+
+  printf("Digite a letra do Estado (A a H): \n");
+  scanf(" %c", &Estado);
+
+  printf("Digite o codigo da carta (Ex: A01): \n");
+  scanf("%s", Cod_carta);
+
   printf("Digite o tamanho da população: \n");
   scanf("%d", &Populacao);
 
@@ -27,9 +37,16 @@ int main() {
   printf("Existem quantos pontos Turisticos na cidade? \n");
   scanf("%d", &Num_pontos_P);
 
-
-
   // Área para exibição dos dados da cidade
+
+  //IMPRIMIR OQUE O USUARIO SELECIONOU
+
+  printf("Estado: %c\n", Estado);
+  printf("Codigo da Carta: %s \n", Cod_carta);
+  printf("O tamanho da população é: %d Habitantes \n", Populacao);
+  printf("O tamanho da área é: %.3f M²\n", Area);
+  printf("O PIB da cidade é: R$%f Milhões\n", PIB);
+  printf("A cidade possui: %d pontos turísticos\n", Num_pontos_P);
 
 return 0;
 } 
